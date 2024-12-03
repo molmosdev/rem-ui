c07449ffef049cfa120e4284510a226eafef9950
molmos <molmosdev@gmail.com>
2024-12-03 16:48:51 +0100

    Add semver dependency and enhance publish script for versioning

--- publish.js
+++ publish.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const { exec } = require("child_process");
 const path = require("path");
+const semver = require("semver");
 
 // Path to the package.json of the realm-ui-angular project
 const packageJsonPath = path.join(
@@ -13,10 +14,9 @@ const packageJsonPath = path.join(
 // Read the package.json file
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 
-// Increment the version
-const versionParts = packageJson.version.split(".");
-versionParts[2] = (parseInt(versionParts[2], 10) + 1).toString();
-packageJson.version = versionParts.join(".");
+// Increment the version based on the type of release
+const releaseType = process.argv[2] || "patch"; // default to 'patch' if no argument is provided
+packageJson.version = semver.inc(packageJson.version, releaseType);
 
 // Save the package.json file with the new version
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), "utf8");
