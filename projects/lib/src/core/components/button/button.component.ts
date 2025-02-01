32998c02b8ddfd8823743fbf4aadd8b7c7966db8
molmos <hello@molmos.dev>
2025-02-01 22:18:39 +0100

    Add link support to Button component and bump package version

--- projects/lib/src/core/components/button/button.component.ts
+++ projects/lib/src/core/components/button/button.component.ts
@@ -1,17 +1,23 @@
 import { Component, input, output } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { NgClass, NgTemplateOutlet } from '@angular/common';
 import { Spinner } from '../spinner/spinner.component';
 import { fadeInOutTrigger } from '../../../shared/animations/animations';
 
+interface LinkConfig {
+  href: string;
+  target?: string;
+}
+
 @Component({
   selector: 'r-button',
-  imports: [NgClass, Spinner],
+  imports: [NgClass, Spinner, NgTemplateOutlet],
   templateUrl: './button.component.html',
   animations: [fadeInOutTrigger],
 })
 export class Button {
   type = input<'primary' | 'secondary' | 'ghost'>('primary');
   loading = input<boolean>(false);
-  disabled = input<boolean>(false); // Nuevo input para controlar el estado disabled
+  disabled = input<boolean>(false);
+  link = input<LinkConfig>();
   clickEmitter = output<void>();
 }
