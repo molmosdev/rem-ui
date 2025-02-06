d09741202e9fb05b5b2b4b767a9b5f8fe2fc9b3a
molmos <hello@molmos.dev>
2025-02-06 21:37:33 +0100

    Add button component with styles and update animation trigger for consistency

--- projects/lib/src/core/components/button/button.component.ts
+++ projects/lib/src/core/components/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, input, output } from '@angular/core';
 import { NgClass, NgTemplateOutlet } from '@angular/common';
 import { Spinner } from '../spinner/spinner.component';
-import { fadeInOutTrigger } from '../../../shared/animations/animations';
+import { fadeInFadeOutTrigger } from '../../../shared/animations/animations';
 
 interface LinkConfig {
   href: string;
@@ -12,12 +12,22 @@ interface LinkConfig {
   selector: 'r-button',
   imports: [NgClass, Spinner, NgTemplateOutlet],
   templateUrl: './button.component.html',
-  animations: [fadeInOutTrigger],
+  animations: [fadeInFadeOutTrigger],
+  styleUrl: './button.component.css',
 })
 export class Button {
+  /** The type of the button. */
   type = input<'primary' | 'secondary' | 'ghost'>('primary');
+
+  /** Indicates whether the button is in a loading state. */
   loading = input<boolean>(false);
+
+  /** Indicates whether the button is disabled. */
   disabled = input<boolean>(false);
+
+  /** Configuration for the link, if the button is used as a link. */
   link = input<LinkConfig>();
+
+  /** Event emitted when the button is clicked. */
   clickEmitter = output<void>();
 }
