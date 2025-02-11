21a43129d752f6cf54a9a28320df8d8c56c88c14
molmos <molmos@circontrol.com>
2025-02-11 17:56:57 +0100

    Implement error state animations for text component to enhance visual feedback

--- projects/lib/src/core/components/text/text.component.ts
+++ projects/lib/src/core/components/text/text.component.ts
@@ -1,12 +1,41 @@
 import { Component, computed, input, model, output } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { Spinner } from '../spinner/spinner.component';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'r-text',
   imports: [NgClass, Spinner],
   templateUrl: './text.component.html',
   styleUrl: './text.component.css',
+  animations: [
+    trigger('errorState', [
+      state(
+        'true',
+        style({
+          color: 'var(--error-foreground, #c40000ab)',
+          backgroundColor: 'var(--error, #fff0f0)',
+          borderColor: 'var(--error-foreground, #c40000ab)',
+        })
+      ),
+      state(
+        'false',
+        style({
+          color: 'var(--input-foreground, #798194)',
+          backgroundColor:
+            'color-mix(in srgb, var(--foreground, #798194) 5%, var(--background, #ffffff))',
+          borderColor: 'var(--border-color, transparent)',
+        })
+      ),
+      transition('false <=> true', animate('0.2s')),
+    ]),
+  ],
 })
 export class Text {
   value = model<string | null>(null);
