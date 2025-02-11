4057290403881badef0bcc426cdc6c54ee5ca689
molmos <molmos@circontrol.com>
2025-02-11 17:25:18 +0100

    Enhance switch component with size variations and animations for improved user experience

--- projects/lib/src/core/components/switch/switch.component.ts
+++ projects/lib/src/core/components/switch/switch.component.ts
@@ -1,16 +1,31 @@
 import { Component, input, model, output } from '@angular/core';
 import { NgClass } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'r-switch',
   imports: [NgClass],
   templateUrl: './switch.component.html',
   styleUrl: './switch.component.css',
+  animations: [
+    trigger('switchState', [
+      state('false', style({ transform: 'translateX(0)', opacity: 0.3 })),
+      state('true', style({ transform: 'translateX(100%)' })),
+      transition('false <=> true', animate('0.3s')),
+    ]),
+  ],
 })
 export class Switch {
   value = model(false);
   label = input<string>('');
   changeEmitter = output<boolean>();
+  size = input<string>('default');
 
   /**
    * Toggle the switch value
