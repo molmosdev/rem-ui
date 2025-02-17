4915f6e9b549e41b7efbb5979b617776bb109657
molmos <molmos@circontrol.com>
2025-02-17 10:26:53 +0100

    Update TabsPlaygroundComponent to use signals for tab labels and enhance args configuration

--- projects/docs/src/app/pages/playground/pages/tabs-playground/tabs-playground.component.ts
+++ projects/docs/src/app/pages/playground/pages/tabs-playground/tabs-playground.component.ts
@@ -12,7 +12,7 @@ import { IArg } from '../../interfaces/arg.interface';
         <r-tabs [(selectedValue)]="args()[0].value">
           @for (tab of tabs; track tab) {
             <r-tab [value]="tab.value" [disabled]="tab.disabled">
-              {{ tab.label }}
+              {{ tab.label() }}
             </r-tab>
           }
         </r-tabs>
@@ -22,6 +22,12 @@ import { IArg } from '../../interfaces/arg.interface';
   `,
 })
 export default class TabsPlaygroundComponent {
+  tabs = [
+    { label: signal('Tab 1'), value: 'tab1', disabled: false },
+    { label: signal('Tab 2'), value: 'tab2', disabled: false },
+    { label: signal('Tab 3'), value: 'tab3', disabled: true },
+  ];
+
   args = signal<IArg[]>([
     {
       label: 'Selected Tab',
@@ -33,11 +39,20 @@ export default class TabsPlaygroundComponent {
         { label: 'Tab 3', value: 'tab3' },
       ],
     },
+    {
+      label: 'Tab 1 Label',
+      type: 'text',
+      value: this.tabs[0].label,
+    },
+    {
+      label: 'Tab 2 Label',
+      type: 'text',
+      value: this.tabs[1].label,
+    },
+    {
+      label: 'Tab 3 Label',
+      type: 'text',
+      value: this.tabs[2].label,
+    },
   ]);
-
-  tabs = [
-    { label: 'Tab 1', value: 'tab1', disabled: false },
-    { label: 'Tab 2', value: 'tab2', disabled: false },
-    { label: 'Tab 3', value: 'tab3', disabled: true },
-  ];
 }
