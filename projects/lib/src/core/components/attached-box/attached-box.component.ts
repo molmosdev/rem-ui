417b8214a84fd957095f1b9afbe19fdc89de48ed
molmosdev <hello@molmos.dev>
2025-03-02 12:23:44 +0100

    Enhance attached box component with improved positioning options and visibility handling

--- projects/lib/src/core/components/attached-box/attached-box.component.ts
+++ projects/lib/src/core/components/attached-box/attached-box.component.ts
@@ -12,7 +12,7 @@ import {
   templateUrl: './attached-box.component.html',
   styleUrl: './attached-box.component.css',
 })
-export class AttachedBoxComponent {
+export class AttachedBox {
   /**
    * The trigger element that will be used to open the content.
    */
@@ -26,7 +26,16 @@ export class AttachedBoxComponent {
   /**
    * The position of the content relative to the trigger.
    */
-  position = model<'top' | 'bottom' | 'left' | 'right'>('top');
+  position = model<
+    | 'top-left'
+    | 'top-right'
+    | 'bottom-left'
+    | 'bottom-right'
+    | 'left-top'
+    | 'left-bottom'
+    | 'right-top'
+    | 'right-bottom'
+  >('bottom-left');
 
   /**
    * The visibility of the content.
@@ -47,37 +56,78 @@ export class AttachedBoxComponent {
   }
 
   /**
-   * Calculates the adjusted position of the content relative to the trigger.
-   * @returns The adjusted position ('top', 'bottom', 'left', or 'right').
+   * Closes the content if clicked outside.
    */
-  private calculateAdjustedPosition(): 'top' | 'bottom' | 'left' | 'right' {
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const triggerElement = this.trigger()?.nativeElement;
+    const contentElement = this.content()?.nativeElement;
+    if (
+      triggerElement &&
+      contentElement &&
+      !triggerElement.contains(event.target as Node) &&
+      !contentElement.contains(event.target as Node)
+    ) {
+      this.isContentVisible.set(false);
+    }
+  }
+
+  /**
+   * Calculates the adjusted position of the content relative to the trigger.
+   * @returns The adjusted position.
+   */
+  private calculateAdjustedPosition():
+    | 'top-left'
+    | 'top-right'
+    | 'bottom-left'
+    | 'bottom-right'
+    | 'left-top'
+    | 'left-bottom'
+    | 'right-top'
+    | 'right-bottom' {
     const triggerElement = this.trigger()!.nativeElement;
     const contentElement = this.content()!.nativeElement;
     const triggerRect = triggerElement.getBoundingClientRect();
     const contentRect = contentElement.getBoundingClientRect();
 
     if (
-      this.position() === 'bottom' &&
+      this.position().startsWith('bottom') &&
       triggerRect.bottom + contentRect.height > window.innerHeight
     ) {
-      return 'top';
+      return this.position().replace('bottom', 'top') as
+        | 'top-left'
+        | 'top-right';
     } else if (
-      this.position() === 'top' &&
+      this.position().startsWith('top') &&
       triggerRect.top - contentRect.height < 0
     ) {
-      return 'bottom';
+      return this.position().replace('top', 'bottom') as
+        | 'bottom-left'
+        | 'bottom-right';
     } else if (
-      this.position() === 'right' &&
+      this.position().startsWith('right') &&
       triggerRect.right + contentRect.width > window.innerWidth
     ) {
-      return 'left';
+      return this.position().replace('right', 'left') as
+        | 'left-top'
+        | 'left-bottom';
     } else if (
-      this.position() === 'left' &&
+      this.position().startsWith('left') &&
       triggerRect.left - contentRect.width < 0
     ) {
-      return 'right';
+      return this.position().replace('left', 'right') as
+        | 'right-top'
+        | 'right-bottom';
     }
-    return this.position();
+    return this.position() as
+      | 'top-left'
+      | 'top-right'
+      | 'bottom-left'
+      | 'bottom-right'
+      | 'left-top'
+      | 'left-bottom'
+      | 'right-top'
+      | 'right-bottom';
   }
 
   /**
