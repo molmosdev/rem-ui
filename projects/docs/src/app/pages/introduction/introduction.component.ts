1441d46705c1a86e4626539dd0a33fa29620f65a
molmos <molmos@circontrol.com>
2025-03-19 19:52:59 +0100

    Enhance introduction component: implement search functionality with dynamic options display

--- projects/docs/src/app/pages/introduction/introduction.component.ts
+++ projects/docs/src/app/pages/introduction/introduction.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Search } from '../../../../../lib/src/core/components/search/search.component';
+import { Option } from '../../../../../lib/src/public-api';
 
 @Component({
   selector: 'app-introduction',
-  imports: [],
+  imports: [Search, Option],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.css',
 })
-export default class IntroductionComponent {}
+export default class IntroductionComponent {
+  searchValue = signal<string>('');
+
+  handleSearchChange(value: string) {
+    this.searchValue.set(value);
+  }
+}
