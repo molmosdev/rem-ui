90e40120d58e665c48ea78f8f234d9aec3617fd0
molmosdev <hello@molmos.dev>
2025-03-22 21:23:25 +0100

    Refactor button documentation styles and template; add icon support to button example

--- projects/docs/src/app/features/documentation/pages/button-documentation/button-documentation.component.ts
+++ projects/docs/src/app/features/documentation/pages/button-documentation/button-documentation.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CodeBlockComponent } from '../../../playground/components/code-block/code-block.component';
-import { Button } from '../../../../../../../lib/src/public-api';
+import { Button, Icon } from '../../../../../../../lib/src/public-api';
 
 @Component({
   selector: 'app-button-documentation',
   templateUrl: './button-documentation.component.html',
   styleUrl: './button-documentation.component.css',
-  imports: [CodeBlockComponent, Button],
+  imports: [CodeBlockComponent, Button, Icon],
 })
 export default class ButtonDocumentationComponent {}
