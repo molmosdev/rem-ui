a91cef5b3160cb95408107c2c02b0a6e1bf39791
molmosdev <hello@molmos.dev>
2025-03-25 00:15:17 +0100

    Add ngx-highlightjs for syntax highlighting and update styles

--- projects/docs/src/app/app.config.ts
+++ projects/docs/src/app/app.config.ts
@@ -3,13 +3,13 @@ import {
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
-
 import { routes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHighlightOptions } from 'ngx-highlightjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,5 +23,12 @@ export const appConfig: ApplicationConfig = {
     ),
     provideClientHydration(withEventReplay()),
     provideAnimations(),
+    provideHighlightOptions({
+      coreLibraryLoader: () => import('highlight.js/lib/core'),
+      languages: {
+        typescript: () => import('highlight.js/lib/languages/typescript'),
+        xml: () => import('highlight.js/lib/languages/xml'),
+      },
+    }),
   ],
 };
