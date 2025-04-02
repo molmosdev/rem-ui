34e954710d739310b86851ffe3a267c7dd2f431f
molmosdev <hello@molmos.dev>
2025-04-03 01:49:54 +0200

    Refactor theme toggle functionality in header component to use button group for improved UX

--- projects/docs/src/app/core/components/header/header.component.ts
+++ projects/docs/src/app/core/components/header/header.component.ts
@@ -3,23 +3,26 @@ import { RouterLink } from '@angular/router';
 import { ThemeService } from '../../../../../../lib/src/core/services/theme.service';
 import { ResponsiveService } from '../../../../../../lib/src/core/services/responsive.service';
 import { Button, Icon } from '../../../../../../lib/src/public-api';
+import { ButtonGroup } from '../../../../../../lib/src/core/components/button-group/button-group.component';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, Icon, Button],
+  imports: [RouterLink, Icon, Button, ButtonGroup],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   themeService = inject(ThemeService);
-  readonly isDarkTheme = computed(() => this.themeService.theme() === 'dark');
   responsiveService = inject(ResponsiveService);
+
+  readonly theme = computed(() => this.themeService.theme());
   readonly isTablet = computed(
     () => this.responsiveService.currentDevice() === 'tablet'
   );
-  toggleTheme() {
-    this.themeService.toggleTheme();
-  }
   readonly isMenuVisible = model(false);
   readonly isThemeConfigVisible = model(false);
+
+  applyTheme(theme: 'light' | 'dark' | 'auto'): void {
+    this.themeService.applyTheme(theme);
+  }
 }
