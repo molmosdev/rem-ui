dcccb66971084323e7726db724ccbf2559258c17
molmosdev <hello@molmos.dev>
2025-04-10 12:45:15 +0200

    Enhance introduction component with custom select element and no errors schema

--- projects/docs/src/app/features/introduction/introduction.component.ts
+++ projects/docs/src/app/features/introduction/introduction.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-introduction',
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.css',
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export default class IntroductionComponent {}
