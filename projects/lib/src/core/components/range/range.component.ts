c20c501e2bde2fcca3dd4af376d55f9491fa5e6b
molmos <molmos@circontrol.com>
2025-04-11 20:10:59 +0200

    Refactor range component: adjust slider height and thumb size for improved usability; remove unused properties

--- projects/lib/src/core/components/range/range.component.ts
+++ projects/lib/src/core/components/range/range.component.ts
@@ -3,7 +3,6 @@ import {
   ElementRef,
   inject,
   model,
-  output,
   input,
   AfterViewInit,
 } from '@angular/core';
@@ -13,10 +12,6 @@ import {
   template: ``,
   styleUrl: './range.component.css',
   host: {
-    '[attr.type]': '"range"',
-    '[attr.min]': 'min',
-    '[attr.max]': 'max',
-    '[attr.step]': 'step',
     '[attr.value]': 'value()',
     '[style.max-width]': 'maxWidth()',
     '(input)': 'onInput($event)',
@@ -24,34 +19,14 @@ import {
 })
 export class Range implements AfterViewInit {
   /**
-   * Minimum value of the range.
-   */
-  min = '0';
-
-  /**
-   * Maximum value of the range.
-   */
-  max = '100';
-
-  /**
-   * Step value of the range.
-   */
-  step = '1';
-
-  /**
    * Current value of the range.
    */
-  readonly value = model<number>(50);
+  readonly value = model<string>('0');
 
   /**
    * Maximum width of the range slider.
    */
   readonly maxWidth = input<string>('100%');
-
-  /**
-   * Event emitted when the value changes.
-   */
-  valueChange = output<number>();
 
   /**
    * Reference to the range element.
@@ -62,15 +37,13 @@ export class Range implements AfterViewInit {
    * Initializes the range value after the view is initialized.
    */
   ngAfterViewInit() {
-    this.value.set(Number(this.el.nativeElement.value));
+    this.value.set(this.el.nativeElement.value);
   }
 
   /**
    * Handles the input event to update the value.
    */
   onInput(event: Event) {
-    const newValue = Number((event.target as HTMLInputElement).value);
-    this.value.set(newValue);
-    this.valueChange.emit(newValue);
+    this.value.set((event.target as HTMLInputElement).value);
   }
 }
