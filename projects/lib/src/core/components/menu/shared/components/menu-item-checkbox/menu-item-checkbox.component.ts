252869d9c5dab1bd1e1a275c257d48528edb2f4f
molmosdev <hello@molmos.dev>
2025-04-18 17:11:09 +0200

    Refactor menu item checkbox component: update host directives for improved structure and maintainability; enhance CSS for better styling consistency.

--- projects/lib/src/core/components/menu/shared/components/menu-item-checkbox/menu-item-checkbox.component.ts
+++ projects/lib/src/core/components/menu/shared/components/menu-item-checkbox/menu-item-checkbox.component.ts
@@ -9,6 +9,16 @@ import { Component } from '@angular/core';
   imports: [],
   template: `<ng-content />`,
   styleUrl: './menu-item-checkbox.component.css',
-  hostDirectives: [CdkMenuItemCheckbox],
+  hostDirectives: [
+    {
+      directive: CdkMenuItemCheckbox,
+      inputs: [
+        'cdkMenuItemDisabled: disabled',
+        'cdkMenuitemTypeaheadLabel: typeaheadLabel',
+        'cdkMenuItemChecked: active',
+      ],
+      outputs: ['cdkMenuItemTriggered: triggered'],
+    },
+  ],
 })
 export class MenuItemCheckboxComponent {}
