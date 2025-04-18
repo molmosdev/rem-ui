b4f997b79df4d3915c945bacaae38eced7b6e107
molmosdev <hello@molmos.dev>
2025-04-18 22:09:20 +0200

    refactor: reorganize component imports and simplify header structure

--- projects/docs/src/app/app.component.ts
+++ projects/docs/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from '../../../lib/src/core/services/theme.service';
-import { NavigationComponent } from './core/components/navigation/navigation.component';
 import { HeaderComponent } from './core/components/header/header.component';
+import { NavigationComponent } from './core/components/navigation/navigation.component';
+import { ThemeConfiguratorComponent } from './core/components/theme-configurator/theme-configurator.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    NavigationComponent,
+    HeaderComponent,
+    ThemeConfiguratorComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
